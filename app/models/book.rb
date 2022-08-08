b07cb3b8d050bbec7c9b79e90f26401ebcbc2890
Kat Brandt <joyfulkat11@gmail.com>
2022-08-08 16:15:01 -0600

    Fill seed file.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+  has_many :book_stores
+  has_many :stores, through: :book_stores
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,4 @@
 class Store < ApplicationRecord
+  has_many :book_stores
+  has_many :books, through: :book_stores
 end
